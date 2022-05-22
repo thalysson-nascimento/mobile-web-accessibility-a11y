af0c80467915bb3908140d695fbe4a84e22b4013
Thalysson <thalyssonwg.nascimento@gmail.com>
2022-05-22 17:39:19 -0300

    feat: created page home

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ButtonModule } from '@directives-components/button/button.module';
 import { HomeComponent } from './home.component';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ButtonModule],
   exports: [HomeComponent],
 })
 export class HomeModule {}
